04f3de1d114ab3f31da83195c4f0237838b039c5
LegendarySHT <1138682030@qq.com>
2025-05-28 22:52:03 +0800

    修复了数码管的一个小的显示问题

--- CPU/CPU.runs/synth_1/CPU_top.tcl
+++ CPU/CPU.runs/synth_1/CPU_top.tcl
@@ -16,7 +16,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param xicom.use_bs_reader 1
 create_project -in_memory -part xc7a35tcsg324-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -32,7 +31,7 @@ set_property ip_repo_paths f:/ComputerOrganization/SUSTech_CS202_Computer-Organi
 set_property ip_output_repo f:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 add_files F:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.srcs/sources_1/ip/RAM/dmem32.coe
-add_files f:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.srcs/sources_1/ip/prgrom/test.coe
+add_files F:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.srcs/sources_1/ip/prgrom/test.coe
 read_verilog -library xil_defaultlib {
   F:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.srcs/sources_1/imports/Downloads/ALU.v
   F:/ComputerOrganization/SUSTech_CS202_Computer-Organization_proj/CPU/CPU.srcs/sources_1/new/CPU_state.v
